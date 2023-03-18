fe12dd9079ca74cd79975785bef59764c944a8c4
oleksiikhrypko <alexey.hripko@gmail.com>
2023-03-18 19:18:24 +0100

    [core] topic

--- pkg/storage/topic/topic.go
+++ pkg/storage/topic/topic.go
@@ -1,4 +1,22 @@
 package topic
 
+import "goqueue/pkg/storage/batch"
+
+type DB interface {
+	Get(key []byte) ([]byte, error)
+	Has(key []byte) (bool, error)
+	IsNotFoundErr(err error) bool
+	Write(batch batch.List) error
+}
+
 type Topic struct {
+	db   DB
+	name []byte
+}
+
+func New(db DB, name []byte) *Topic {
+	return &Topic{
+		db:   db,
+		name: name,
+	}
 }
